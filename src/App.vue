c675188e455c424c058acb54b72bd38408c48503
Valdez Gutierrez Aldo Eduardo <avaldez0@ucol.mx>
2025-03-04 18:20:23 -0600

    Add logout functionality to App.vue; implement session clearing and navigation on logout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 import { RouterView } from 'vue-router'
 
-const drawer = ref(false) 
+const drawer = ref(false)
+const router = useRouter()
+
+const cerrarSesion = () => {
+  localStorage.removeItem('id_alumno')
+  router.push('/') 
+}
 </script>
 
 <template>
   <v-app>
-
     <v-app-bar app color="green-darken-3" density="comfortable" elevation="4">
       <v-container class="d-flex align-center">
         <v-img src="@/assets/logo.svg" alt="Vue logo" max-height="40" max-width="40" />
@@ -17,6 +23,7 @@ const drawer = ref(false)
         <div class="hidden-sm-and-down">
           <v-btn variant="text" to="/" exact color="white">Inicio</v-btn>
           <v-btn variant="text" to="/crear_cuento" color="white">Crear Cuento</v-btn>
+          <v-btn variant="text" @click="cerrarSesion"> Cerrar Sesión</v-btn>
         </div>
 
         <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = !drawer" />
@@ -27,6 +34,11 @@ const drawer = ref(false)
       <v-list>
         <v-list-item to="/" exact title="Inicio" />
         <v-list-item to="/crear_cuento" title="Crear Cuento" />
+        <v-list-item title="Cerrar Sesión" @click="cerrarSesion">
+          <template v-slot:prepend>
+            <v-icon color="red">mdi-logout</v-icon>
+          </template>
+        </v-list-item>
       </v-list>
     </v-navigation-drawer>
 
