e7ee4a00b5fa8f5c8bde65a9421bfea44cf0a2da
Gaia74 <gaia745mocte@gmail.com>
2025-05-26 03:45:24 -0600

    Button for profile added

--- src/components/AppNavbarWhite.vue
+++ src/components/AppNavbarWhite.vue
@@ -1,12 +1,41 @@
 <template>
   <v-app-bar app class="app-navbar">
     <LogoCollabtalesSm/>
+
+    <v-spacer></v-spacer>
+
+    <v-menu>
+      <template v-slot:activator="{ props }">
+        <v-btn
+          icon
+          v-bind="props" >
+          <v-img
+            src="/icons/account.svg"
+            alt="Perfil icono"
+            width="40"
+            height="40"
+            class="icon"
+          ></v-img>
+        </v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item @click="router.push('/perfil_usuario')">
+          <v-list-item-title class="font-weight-bold">Ver Mi Perfil</v-list-item-title>
+        </v-list-item>
+        <v-list-item @click="logout" class="text-red-darken-3">
+          <v-list-item-title>Cerrar Sesión</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
   </v-app-bar>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
 import LogoCollabtalesSm from '@/components/icons/LogoCollabtalesSm.vue';
+import { useRouter } from 'vue-router';
+const router = useRouter();
 
 const backgroundColor = ref('');
 
@@ -14,4 +43,9 @@ onMounted(() => {
   const color = getComputedStyle(document.documentElement).getPropertyValue('--color-navbar-white').trim();
   backgroundColor.value = color;
 });
+
+function logout() {
+  router.push('/'); // Redirect to login page
+  alert('Has cerrado sesión.');
+}
 </script>
